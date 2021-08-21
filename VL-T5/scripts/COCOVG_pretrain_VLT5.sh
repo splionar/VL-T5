cb4f52f978063fb9ca6ef9250cbc2acc8823de51
Stefan Lionar <31429575+splionar@users.noreply.github.com>
2021-08-21 11:20:16 +0200

    Update COCOVG_pretrain_VLT5.sh

--- VL-T5/scripts/COCOVG_pretrain_VLT5.sh
+++ VL-T5/scripts/COCOVG_pretrain_VLT5.sh
@@ -10,7 +10,7 @@ python -m torch.distributed.launch \
         --distributed --multiGPU --fp16 \
         --train mscoco_resplit_train \
         --valid mscoco_resplit_val \
-        --batch_size 8 \
+        --batch_size 16 \
         --optim adamw \
         --warmup_ratio 0.05 \
         --lr 1e-4 \
